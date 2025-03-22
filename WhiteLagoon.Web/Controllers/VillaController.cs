1d2a74b6b83abef2fb26b1e069270c1f3c4c22b4
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-03-22 22:02:11 +0530

    Create a new Controller for Villa

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        //ApplicationDbContext app = new ApplicationDbContext()
+        //{
+        //    // use this in net framework
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
