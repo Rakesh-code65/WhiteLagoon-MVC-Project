71fe78ee58085fab247824846f0bc70e0e2d069b
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-03-24 11:26:23 +0530

    In the data annotation increase the occupancy limit.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -17,7 +17,7 @@ namespace WhiteLagoon.Domain.Entities
         [Range(10,10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
-        [Range(1,10)]
+        [Range(1,100)]
         public int Occupancy { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
