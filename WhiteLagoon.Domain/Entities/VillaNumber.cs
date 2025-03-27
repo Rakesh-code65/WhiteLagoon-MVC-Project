305e863ccbf775ee1c5ca7e9dade157b5c89c696
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-03-27 11:06:32 +0530

    Created a  villa Number Table and seed records.

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -12,6 +12,7 @@ namespace WhiteLagoon.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Villa Number")]
         public int Villa_Number { get; set; }
 
         [ForeignKey("Villa")]
