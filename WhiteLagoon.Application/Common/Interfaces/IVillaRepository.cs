2edf1db4a2dac4b34d68f90331496cee537be378
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-04-01 11:35:59 +0530

    Implemented Villa Repository and interface,

---
+++ WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Application.Common.Interfaces;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll(Expression<Func<IVilla,bool>>? filter = null,string? inlcudeProperties = null);
+        IEnumerable<Villa> GetAll(Expression<Func<IVilla, bool>> filter, string? inlcudeProperties = null);
+        void Add(Villa entity);
+        void Update(Villa entity);
+        void Remove(Villa entity);
+        void Save();
+    }
+}
--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
