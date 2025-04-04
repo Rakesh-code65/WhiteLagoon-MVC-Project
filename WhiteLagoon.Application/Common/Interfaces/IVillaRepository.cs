d424900345844731655e7f04f8d2847f76d94ceb
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-04-04 11:31:18 +0530

    Added a null validation here.

--- WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
+++ WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
@@ -11,8 +11,8 @@ namespace WhiteLagoon.Application.Common.Interfaces
 {
     public interface IVillaRepository
     {
-        Villa Get(Expression<Func<Villa,bool>>? filter = null,string? inlcudeProperties = null);
-        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>> filter, string? inlcudeProperties = null);
+        Villa Get(Expression<Func<Villa, bool>>? filter = null, string? inlcudeProperties = null);
+        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? inlcudeProperties = null);
         void Add(Villa entity);
         void Update(Villa entity);
         void Remove(Villa entity);
