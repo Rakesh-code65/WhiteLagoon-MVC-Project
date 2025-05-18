64d46f7c4e77cdb17c7bc72802e3dcb5f172e429
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-05-18 15:21:19 +0530

    Added a Notmapped Annotation in this villa class.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace WhiteLagoon.Domain.Entities
 {
@@ -19,6 +22,12 @@ namespace WhiteLagoon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1,100)]
         public int Occupancy { get; set; }
+
+        // Not mapped is another data annotation that can be used on entities when you add
+        // not mapped we are telling entity framework that the columns that we are adding here you you do not have to add that to the database that way it will not try to 
+        // add the image column to the database and that is exactly what we want.
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
