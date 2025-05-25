f8fe93066dea3dd241854a5aa06f8cb3384729a6
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-05-26 00:05:52 +0530

    Created a Amenity Repository

---
+++ WhiteLagoon.Infrastructure/Repository/AmenityRepository - Copy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+        // IvillaNumberRepository is interface here.
+    {
+       
+           
+            private readonly ApplicationDbContext _db;
+
+            public AmenityRepository(ApplicationDbContext db) : base(db)
+            {
+                _db = db;
+            }
+        //    public void Add(Villa entity)
+        //{
+        //        _db.Add(entity);
+        //}
+        //we are moving these codes with a generic repository and we are using the generic repository in the controller. and then implement that in villa repository.
+
+        //public Villa Get(Expression<Func<Villa, bool>>? filter, string? inlcudeProperties = null)
+        //{
+
+        //    IQueryable<Villa> query = _db.Set<Villa>();
+        //    if (filter != null)
+        //    {
+        //        query = query.Where(filter);
+        //    }
+        //    if (!string.IsNullOrEmpty(inlcudeProperties))
+        //    {
+        //        //Villa, VillaNumber - case sensitive
+        //        foreach (var includeProp in inlcudeProperties.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries))
+        //        {
+        //            query = query.Include(includeProp);
+        //        }
+        //    }
+        //    return query.FirstOrDefault();
+        //}
+        //public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? inlcudeProperties = null)
+        //{
+        //    IQueryable<Villa> query = _db.Set<Villa>();
+        //    if(filter!=null)
+        //    {
+        //        query = query.Where(filter);
+        //    }
+        //    if (!string.IsNullOrEmpty(inlcudeProperties))
+        //    {
+        //        //Villa, VillaNumber - case sensitive
+        //        foreach (var includeProp in inlcudeProperties.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries))
+        //        {
+        //            query = query.Include(includeProp);
+        //        }
+        //    }
+        //    return query.ToList();
+        //}
+
+        //public void Remove(Villa entity)
+        //{
+        //        _db.Remove(entity);
+        //}
+
+        //public void Save()
+        //{
+        //    _db.SaveChanges();
+        //}
+
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+
+}
--- WhiteLagoon.Infrastructure/Repository/UnitofWork.cs
+++ WhiteLagoon.Infrastructure/Repository/UnitofWork.cs
@@ -12,12 +12,13 @@ namespace WhiteLagoon.Infrastructure.Repository
     {
         private readonly ApplicationDbContext _db;
         public IVillaRepository Villa { get; private set; }
-
+        public IAmenityRepository Amenity { get; private set; }
         public IVillaNumberRepository VillaNumber { get; private set; }
         public UnitofWork(ApplicationDbContext db)
         {
             _db = db;
             Villa = new VillaRepository(_db);
+            Amenity = new AmenityRepository(_db);
             VillaNumber = new VillaNumberRepository(_db);
         }
 
