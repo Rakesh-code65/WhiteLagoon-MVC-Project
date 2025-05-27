1673c126806e799348ec59647dc49d27f83fd1de
Rakesh <72179027+Rakesh-code65@users.noreply.github.com>
2025-05-27 13:27:10 +0530

    Add Dropdown in Navbar in Homecontroller and created a home view model.

---
+++ WhiteLagoon.Web/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WhiteLagoon.Web.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
